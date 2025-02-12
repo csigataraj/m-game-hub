9acadbbeff5628ab03f5ba18c074b7a295ffca0b
Attila Batky <attila.batky@bt.com>
2025-02-12 16:08:37 +0100

    display genres with icons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useFetchGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useFetchGenres();
   return (
-    <ul>
+    <List>
       {data.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <ListItem key={item.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(item.image_background)}
+            />
+            <Text fontSize="lg">{item.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
