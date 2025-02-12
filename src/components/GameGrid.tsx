c0e6262e7fc7858116f989cc70d6c196ee1393fb
Attila Batky <attila.batky@bt.com>
2025-02-12 15:00:29 +0100

    Add loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,9 +9,11 @@ import {
 } from "@chakra-ui/react";
 import useFetchGames from "../hooks/fetch-games-hook";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useFetchGames();
+  const { games, error, isLoading } = useFetchGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -21,6 +23,7 @@ const GameGrid = () => {
         spacing={10}
         padding="10px"
       >
+        {isLoading && skeletons.map((item) => <GameCardSkeleton key={item} />)}
         {games.map((item) => (
           <GameCard key={item.id} game={item} />
         ))}
--- src/hooks/fetch-games-hook.tsx
+++ src/hooks/fetch-games-hook.tsx
@@ -4,23 +4,27 @@ import create from "../services/http-service";
 import { Game, GameResponse } from "../interfaces/game";
 
 const useFetchGames = () => {
+  const [isLoading, setLoading] = useState(false);
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
   const API = create(RAWG_API_URL, RAWG_API_KEY);
 
   useEffect(() => {
+    setLoading(true);
     API.get<GameResponse>()
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
         console.log(res.data.results);
       })
       .catch((err) => {
         setGames([]);
+        setLoading(false);
         setError(err.message);
       });
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useFetchGames;
